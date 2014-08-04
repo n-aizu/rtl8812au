f227ecc44c94931a3a99e0da9ba5013013501220
Naoki Aizu <naoki.aizu@gmail.com>
2014-08-05 00:29:21 +0900

    Add support Planex GW-450S(SHURIKEN)

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -314,6 +314,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(0x2001, 0x3314),.driver_info = RTL8821}, /* D-Link - Cameo */
 	{USB_DEVICE(0x2001, 0x3318),.driver_info = RTL8821}, /* D-Link - Cameo */
 	{USB_DEVICE(0x0E66, 0x0023),.driver_info = RTL8821}, /* HAWKING - Edimax */
+	{USB_DEVICE(0x2019, 0xAB32),.driver_info = RTL8821}, /* Planex - Abocom */
 #endif
 
 #ifdef CONFIG_RTL8192E
